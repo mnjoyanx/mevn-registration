d811652640fc804f540eb512fc9ef0e187de0617
mnjoyanx <tigranmnjoyan@gmail.com>
2021-05-01 18:45:36 +0400

    error handler

--- controllers/register.js
+++ controllers/register.js
@@ -1,17 +1,16 @@
 const User = require("../models/user")
-const { ValidationError } = require('sequelize')
 const bcrypt = require('bcrypt')
 
 async function registration(req, res) {
-    const { username, email } = req.body
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    try {
+    const { username, email, password } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
+    try { 
         const newuser = await User.create({ username, email, password: hashedPassword })
-        res.json(newuser)
-    } catch (err) {
-        res.json(err.errors[0].message)
-        console.log(err);
-    }
-}
-
-module.exports = registration
+        res.json(newuser) 
+    } catch (err) { 
+        const errorHandler = err.errors.map(item => item.message)
+        res.json(errorHandler) 
+    } 
+} 
+ 
+module.exports = registration   
